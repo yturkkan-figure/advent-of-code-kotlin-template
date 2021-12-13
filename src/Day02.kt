8c7bd9bfeba3f31833a0df822ad52add02ec1a0d
yturkkan-figure <yturkkan@figure.com>
2021-12-12 19:36:01 -0800

    Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,47 +1,54 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        var count = 0
-        var cur: Int = input[0].toInt()
-        for (i in 1..input.size-1){
-            if (input[i].toInt() > cur){
-                count += 1
-            }
-            cur = input[i].toInt()
-            if (i == input.size-1){
-                break
+        var depth = 0
+        var horizontal = 0
+
+        for (i in input){
+            val lineList = i.split(" ")
+            val direction = lineList[0]
+            val amount = lineList[1].toInt()
+
+            when(direction){
+                "forward" -> horizontal += amount
+                "down" -> depth += amount
+                "up" -> depth -= amount
             }
         }
-
-        return count
+        return depth * horizontal
     }
 
     fun part2(input: List<String>): Int {
-        var count = 0
-        var cur: Int
-        if (input.size < 3) { return 0 }
-        else { cur = input[0].toInt() + input[1].toInt() + input[2].toInt()  }
+        var depth = 0
+        var horizontal = 0
+        var aim = 0
 
-        for (i in 3..input.size-1){ //1,2,3,4,5,6
-            val next = cur - input[i - 3].toInt() + input[i].toInt()
-            if (next > cur){
-                count += 1
+        for (i in input){
+            val lineList = i.split(" ")
+            val direction = lineList[0]
+            val amount = lineList[1].toInt()
+
+            if (direction == "forward" && aim == 0) {
+                horizontal += amount
+            } else if (direction == "forward") {
+                horizontal += amount
+                depth += amount * aim
+            } else if (direction == "down") {
+                aim += amount
+            } else if (direction == "up") {
+                aim -= amount
             }
-            cur = next
-            if (i == input.size-1){
-                break
-            }
+
         }
-
-        return count
+        return depth * horizontal
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 7)
-    check(part2(testInput) == 5)
+    val testInput = readInput("Day02_test")
+//    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
 
-    val input = readInput("Day01")
-    println(part1(input))
+    val input = readInput("Day02")
+//    println(part1(input))
     println(part2(input))
 }
